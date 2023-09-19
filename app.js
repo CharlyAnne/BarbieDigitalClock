00f5ae0b221be7fe70a1a1158e0c3b842a4dfffc
Charly <icharlieanne@gmail.com>
2023-09-19 19:30:20 +0100

    added widget

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function showTime() {
   var h = date.getHours(); // 0 - 23
   var m = date.getMinutes(); // 0 - 59
   var s = date.getSeconds(); // 0 - 59
-  var session = "AM";
+  var session = 'AM';
 
   if (h == 0) {
     h = 12;
@@ -11,17 +11,25 @@ function showTime() {
 
   if (h > 12) {
     h = h - 12;
-    session = "PM";
+    session = 'PM';
   }
 
-  h = (h < 10) ? "0" + h : h;
-  m = (m < 10) ? "0" + m : m;
-  s = (s < 10) ? "0" + s : s;
+  h = h < 10 ? '0' + h : h;
+  m = m < 10 ? '0' + m : m;
+  s = s < 10 ? '0' + s : s;
 
-  var time = h + ":" + m + ":" + s + " " + session;
-  document.getElementById("MyClockDisplay").innerText = time;
-  document.getElementById("MyClockDisplay").textContent = time;
+  var time = h + ':' + m + ':' + s + ' ' + session;
+  document.getElementById('MyClockDisplay').innerText = time;
+  document.getElementById('MyClockDisplay').textContent = time;
   setTimeout(showTime, 1000);
 }
 
 showTime();
+
+//Widget
+function toggleChat() {
+  var chatIcon = document.getElementById('chat-icon');
+  var chatContent = document.querySelector('.chat-content');
+
+  chatContent.classList.toggle('hidden');
+}
